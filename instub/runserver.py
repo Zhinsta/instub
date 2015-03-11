f79184e217cc6762b6300ab5b16cdb943e87f372
asaka <aisk1988@gmail.com>
2015-03-11 10:49:29 +0800

    use gevent to run server

--- instub/runserver.py
+++ instub/runserver.py
@@ -1,10 +1,18 @@
 # coding: utf-8
 
+from gevent import monkey
+monkey.patch_all()  # NOQA
+
+from gevent.wsgi import WSGIServer
+
 from instub.app import app
 
 
 def main():
-    app.run(host='localhost', port=5000, debug=app.config['DEBUG'])
+    # app.run(host='localhost', port=5000, debug=app.config['DEBUG'])
+    app.debug = app.config['DEBUG']
+    http_server = WSGIServer(('localhost', 6000), app)
+    http_server.serve_forever()
 
 if __name__ == '__main__':
     main()
