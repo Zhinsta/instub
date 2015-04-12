e3bd6a5b75c0b2cce524b7a8c38471a7b9abf20b
goodspeed <cacique1103@gmail.com>
2015-04-12 15:54:41 +0800

    修改time since

--- instub/filters.py
+++ instub/filters.py
@@ -35,7 +35,7 @@ def image(url):
 
 
 @app.template_filter()
-def time_since(dt, default='刚刚'):
+def time_since(dt, default='just now'):
     """将 datetime 替换成字符串 ('3小时前', '2天前' 等等)
     的 Jinja filter copy from
     https://github.com/tonyblundell/socialdump/blob/master/socialdump.py
@@ -50,17 +50,17 @@ def time_since(dt, default='刚刚'):
     if total_seconds > 0:
         if total_seconds < 315360000:
             periods = (
-                (total_seconds / 86400, '天'),
-                (total_seconds / 3600, '小时'),
-                (total_seconds / 60, '分钟'),
-                (total_seconds, '秒'),
+                (total_seconds / 86400, 'days'),
+                (total_seconds / 3600, 'hours'),
+                (total_seconds / 60, 'minites'),
+                (total_seconds, 'seconds'),
             )
             for period, unit in periods:
                 if period > 0:
-                    return '%d%s前' % (period, unit)
+                    return '%d %s ago' % (period, unit)
             return default
         elif total_seconds < 86400 and dt.day == now.day:
-            return '今天 ' + dt.strftime('%H:%M')
+            return 'today ' + dt.strftime('%H:%M')
         else:
             return unicode(dt.strftime('%Y-%m-%d %H:%M'))
     else:
